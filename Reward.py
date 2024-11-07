ecd5c1ba40d3b1e10285a7c093c25435510ead34
Dip Bhowmick <62492915+dipbhowmick@users.noreply.github.com>
2024-11-07 17:49:42 +0530

    Update Reward.py

--- Reward.py
+++ Reward.py
@@ -9,6 +9,14 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 pc_search = 2
 mobile_search = 2
 
+def getUrl(driver, url, sleep=2, retry=3):
+    for _ in range(retry):
+        try:
+            driver.get(url)
+            break
+        except Exception as e:
+            print(f"[GET ERROR {url}] {e}")
+
 _driver_path = EdgeChromiumDriverManager().install()
 _options = Options()
 _options.add_argument("--headless")
@@ -16,10 +24,9 @@ _options.add_argument("--disable-gpu")
 _options.add_argument("--no-sandbox")
 _options.add_argument(f"user-data-dir={os.path.join(os.path.dirname(os.path.abspath(__file__)), 'user_data')}")
 driver = webdriver.Edge(_options, Service(_driver_path))
+driver.set_page_load_timeout(10)
 
-driver.get('https://rewards.bing.com/')
-time.sleep(2)
-
+getUrl(driver, 'https://rewards.bing.com/')
 print(driver.title)
 
 if "sign in" in driver.title.lower():
@@ -44,15 +51,14 @@ for a in driver.find_elements(By.XPATH, "//div[@id='more-activities']//a[contain
     a.click()
 time.sleep(2)
 
-for tab in driver.window_handles:
-    if tab != _tab1:
-        driver.switch_to.window(tab)
-        driver.close()
-driver.switch_to.window(_tab1)
+# for tab in driver.window_handles:
+#     if tab != _tab1:
+#         driver.switch_to.window(tab)
+#         driver.close()
+# driver.switch_to.window(_tab1)
 
 for _ in range(pc_search):
-    driver.get(f"https://www.bing.com/search?q={''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
-    time.sleep(2)
+    getUrl(driver, f"https://www.bing.com/search?q={''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
 
 dev_tools = driver.execute_cdp_cmd
 dev_tools('Emulation.setDeviceMetricsOverride', {
@@ -66,14 +72,11 @@ dev_tools('Network.setUserAgentOverride', {
     'userAgent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
     'deviceScaleFactor': 2,
 })
-driver.refresh()
 
 for _ in range(mobile_search):
-    driver.get(f"https://www.bing.com/search?q={''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
-    time.sleep(2)
+    getUrl(driver, f"https://www.bing.com/search?q={''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
 
-driver.get('https://rewards.bing.com/')
-time.sleep(2)
+getUrl(driver, 'https://rewards.bing.com/')
 
 user = driver.find_element(By.ID, 'mectrl_currentAccount_primary').get_attribute('textContent').strip()
 total = int(driver.find_element(By.ID, 'balanceToolTipDiv').text.split()[-1])
