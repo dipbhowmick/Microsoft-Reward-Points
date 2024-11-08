c5c21f76936eb25225cd12ee2e61c51dfbbdbd24
Dip Bhowmick <62492915+dipbhowmick@users.noreply.github.com>
2024-11-08 15:11:17 +0530

    Update Reward.py

--- Reward.py
+++ Reward.py
@@ -10,6 +10,8 @@ username = sys.argv[1]
 password = sys.argv[2]
 pc_search = 40
 mobile_search = 2
+with open('words.txt', 'r') as f:
+    words = f.read().split()
 
 def getUrl(driver, url, sleep=3, retry=3):
     for _ in range(retry):
@@ -72,7 +74,7 @@ for _href in _a_href:
 driver.switch_to.window(_tab1)
 
 for _ in range(pc_search):
-    getUrl(driver, f"https://www.bing.com/search?q={''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
+    getUrl(driver, f"https://www.bing.com/search?q={random.choice(words)}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
 
 dev_tools = driver.execute_cdp_cmd
 dev_tools('Emulation.setDeviceMetricsOverride', {
@@ -88,7 +90,7 @@ dev_tools('Network.setUserAgentOverride', {
 })
 
 for _ in range(mobile_search):
-    getUrl(driver, f"https://www.bing.com/search?q={''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
+    getUrl(driver, f"https://www.bing.com/search?q={random.choice(words)}&form=QBLH&sp=-1&ghc=1&lq=0&pq=hel&sc=10-3&qs=n&sk=&cvid=CA3534C5DE96458BB6A7C71DDAD1EC80&ghsh=0&ghacc=0&ghpl=")
 
 getUrl(driver, 'https://rewards.bing.com/')
 
@@ -97,7 +99,10 @@ total = int(driver.find_element(By.ID, 'balanceToolTipDiv').get_attribute('textC
 today = int(driver.find_element(By.ID, 'dailypointToolTipDiv').get_attribute('textContent').split()[-1])
 print(F"[{user}] Total: {total} Today: {today}")
 
-body = f"[{user}]\nTotal: {total-_total} [{_total}->{total}]\nToday: {today-_today} [{_today}->{today}]"
+getUrl(driver, 'https://rewards.bing.com/pointsbreakdown')
+_status = driver.find_elements(By.XPATH, "//div[@id='earningreport-level-heading']/p").get_attribute('textContent').strip()
+
+body = f"[{user}]\nTotal: {total-_total} [{_total}->{total}]\nToday: {today-_today} [{_today}->{today}\nStatus: {_status}]"
 qbody = urllib.parse.quote(body)
 getUrl(driver, f"https://script.google.com/macros/s/AKfycbxQJ8fPbmXHu2qKO8gCE_5ezf-0CH0s7jXDGbRarJVsuTNlNzMoIVfr_wiXy73DC-Ob/exec?email=bhowmickdip8@gmail.com&sub=Microsoft-Reward-Points&body={qbody}")
 print(driver.find_element(By.TAG_NAME, 'body').text)
